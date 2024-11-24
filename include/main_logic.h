7e1fe8d1f19475969d60ba2c6a4f24e8def529d2
Hussam Alkhafaji <alkhafaji.hussam@outlook.com>
2024-11-24 01:34:51 +0100

    modularized main for easier unit testing

---
+++ include/main_logic.h
@@ -0,0 +1,17 @@
+
+/**
+ * @brief prints out the CLI app usage message
+ *
+ * @param argv[] arguments vector
+ */
+void print_usage(char *argv[]);
+
+/**
+ * @brief contains the main logic controller of the CLI app
+ *
+ * @param argc arguments count
+ * @param argv[] arguments vector
+ *
+ * @return int -> StatusCode
+ */
+int process_arguments(int argc, char *argv[]);
---
+++ src/main_logic.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "context.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) 
+{
+	printf("Usage: %s -n -f <db file>\n", argv[0]);
+	printf("\t -n - create new db file\n");
+	printf("\t -f \"path\" - (required) path to db file\n");
+	printf("\t -a \"name,address,hours\" - new entry to add to db\n");
+	printf("\t -d \"name\" - delete employee \"name\" from database file\n");
+	printf("\t -u \"name\" -v hours - update employee \"name\" hours\n");
+	return;
+}
+
+int process_arguments(int argc, char *argv[])
+{
+	int c = 0;
+
+	ProgramOptions opts = {0};
+	opts.newfile = false;
+	opts.ls_employees = false;
+	opts.del = false;
+	opts.update = false;
+	opts.input_hours = -1;
+
+	DBContext ctx = {0};
+	ctx.db_fd = -1;
+
+	int status = STATUS_OK;
+	
+	// Prologue
+	while ((c= getopt(argc, argv, "nf:a:ld:u:v:")) != -1) 
+	{
+			switch(c)
+			{
+			case 'n':
+				opts.newfile = true;
+				break;
+			case 'f':
+				opts.filepath = optarg;
+				break;
+			case 'a':
+				opts.newinput = optarg;
+				opts.newinput[DB_INPUT_MAX_SIZE-1] = '\0';
+				break;
+			case 'l':
+				opts.ls_employees = true;
+				break;
+			case 'd':
+				opts.search_name = optarg;
+				opts.search_name[NAME_SIZE-1] = '\0';
+				opts.del = true;
+				break;
+			case 'u':
+				opts.search_name = optarg;
+				opts.search_name[NAME_SIZE-1] = '\0';
+				opts.update = true;
+				break;
+			case 'v':
+				opts.input_hours = atoi(optarg);
+				break;
+			case '?':
+				printf("Unknown argument -%c\n", c);
+				print_usage(argv);
+				return STATUS_INVALID_ARGUMENT;
+			default:
+				perror("getopt");
+				return STATUS_INVALID_ARGUMENT;
+			}
+	}
+
+	if(!opts.filepath){
+		printf("Path is a required argument\n");
+		print_usage(argv);
+		return STATUS_FILE_NOT_FOUND;
+	}
+
+	// File handling	
+	if (status == STATUS_OK && opts.newfile)
+	{
+		status = create_db_file(opts.filepath, &ctx.db_fd);
+		if(status == STATUS_OK)
+		{
+			status = create_db_header(&ctx);
+		}
+		if (status == STATUS_FILE_EXISTS)
+		{
+			printf("File already exists\n");
+			status = STATUS_OK; // continue with existing file
+			opts.newfile = false;
+		}
+	} 
+	
+	if (status == STATUS_OK && opts.filepath && !opts.newfile)
+	{
+		status = open_db_file(opts.filepath, &ctx.db_fd);
+		if (status == STATUS_OK)
+		{
+			status = validate_db_header(&ctx);
+		}
+		if (status == STATUS_OK )
+		{
+	    	status = read_employees(&ctx);
+		}
+	}	
+
+
+	// State change operations
+	if (status == STATUS_OK && opts.newinput)
+	{
+		status = add_employee(&ctx, opts.newinput);	
+		if (status == STATUS_OK)
+		{
+			printf("Employee %s added.\n", opts.newinput);
+		}
+	}
+
+	if (status == STATUS_OK && opts.update)
+	{
+		status = update_employee_hours_by_name(&ctx, opts.search_name, opts.input_hours);
+		if (status == STATUS_OK)
+		{
+			printf("Employee %s hours updated.\n", opts.search_name);
+		}
+	}
+
+	if (status == STATUS_OK && opts.del)
+	{
+		int status = del_employee_by_name(&ctx, opts.search_name);
+		if (status == STATUS_OK)
+		{
+			printf("Employee %s deleted.\n", opts.search_name);
+		}
+	}
+
+	// List operations
+	if (status == STATUS_OK && opts.ls_employees)
+	{
+		if (ctx.employees == NULL)
+		{
+			printf("No employees in database to list.\n");
+		}else
+		{
+			list_employees(&ctx);
+		}
+	}
+
+	// Epilogue
+	if (status == STATUS_OK)
+	{
+		status = output_file(&ctx); 
+	}
+
+	cleanup_context(&ctx);
+	return status;
+}
