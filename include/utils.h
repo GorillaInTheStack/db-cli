40b0db09a8423dec80b15f9b80cc0ec2d9f10b43
Hussam Alkhafaji <alkhafaji.hussam@outlook.com>
2024-11-24 16:17:03 +0100

    added util to get max fd in env

---
+++ include/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <sys/resource.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long get_max_fd();
+
+#endif
+
---
+++ src/utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+unsigned long get_max_fd() {
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_NOFILE, &limit) != 0) {
+        perror("getrlimit");
+        exit(EXIT_FAILURE);
+    }
+    return limit.rlim_cur;
+}
+
